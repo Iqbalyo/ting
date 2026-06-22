519b6f480faafd33f4292544eb177d620bd28afb
Iqbal <iqbalmustakim734@gmail.com>
2026-06-22 10:44:53 +0700

    update Authservice

--- ting-backend/app/Http/Controllers/AuthController.php
+++ ting-backend/app/Http/Controllers/AuthController.php
@@ -2,17 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
 use App\Services\AuthService; // ini namanya **Dependencies Injection**
 
-//jadi tidak perlu bikin new AuthService(); karena
+//jadi tidak perlu bikin new AuthService(); karena bakalan urus oleh Laravel services container
 
 class AuthController extends Controller
 {
     //
-    public function __construct(private AuthService $authServices
-    )
+    public function __construct(
+        private AuthService $authService
+    ) {}
+
+    public function login(LoginRequest $request)
     {
-       
+        $validated = $request->validated();
+        return $this->authService->login($validated);
     }
 }
--- ting-backend/app/Services/AuthService.php
+++ ting-backend/app/Services/AuthService.php
@@ -2,7 +2,21 @@
 
 namespace App\Services;
 
+use App\Models\User;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
+
 class AuthService
 {
     //
+    public function login(array $validated)
+    {
+        $user = User::where('email', $validated['email'])->first();
+
+        if (! $user) {
+            throw ValidationException::withMessages([
+                'email' => ['Email atau password salah']  //kenapa menggunakan array [],karena Laravel tetap menggunakan array agar formatnya konsisten.
+            ]);
+        }
+    }
 }
